2db1dddeff3c3a5180c5745ea1c2b23720be1881
Raghav Jethaliya <rjjethaliya853@gmail.com>
2023-08-20 14:25:20 +0530

    Listingplaces

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,17 +1,18 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import {Link} from "react-router-dom";
 
 export default function IndexPage() {
     const [places, setPLaces] = useState([]);
     useEffect(() => {
         axios.get('/places').then(response => {
-            setPLaces([...response.data,...response.data,...response.data,...response.data]);
+            setPLaces(response.data);
         })
     }, []);
     return(
         <div className="mt-8 grid gap-x-6  gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
             {places.length > 0 && places.map(place => (
-                <div>
+                <Link to={'/place/'+place._id}>
                     <div className="bg-gray-500 mb-2 rounded-2xl flex">
                         {place.photos?.[0] && (
                             <img className="rounded-2xl object-cover aspect-square" src={'http://localhost:4000/uploads/'+place.photos?.[0]} alt="" />
@@ -22,7 +23,7 @@ export default function IndexPage() {
                     <div className="mt-0.5">
                         <span className="font-bold">₹{place.price} </span> per night
                     </div>
-                </div>
+                </Link>
             ))}
      </div>
     );
