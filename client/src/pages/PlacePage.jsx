62a8f430e626e106fc0dd46589c63556100bf849
Raghav Jethaliya <rjjethaliya853@gmail.com>
2023-08-23 22:12:51 +0530

    SinglePlace#3

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -40,9 +40,15 @@ export default function PlacePage() {
     return(
         <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
             <h1 className="text-3xl">{place.title}</h1>
-            <a className="my-2 block font-semibold underline" target="_blank" href={"https:/maps.google.com/?q="+place.address}>{place.address}</a>
+            <a className="flex gap-1 my-2 font-semibold underline" target="_blank" href={"https:/maps.google.com/?q="+place.address}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                </svg>
+                {place.address}
+                </a>
             <div className="relative">
-                <div className="grid gap-2 grid-cols-[2fr_1fr]">
+                <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
                     <div>
                         {place.photos?.[0] && (
                             <div>
@@ -62,13 +68,47 @@ export default function PlacePage() {
                         </div>
                     </div>
                 </div>
-                <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 bg-white rounded-2xl shadow shadow-md shadow-gray-500  right-2 py-2 px-4">
+                <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 bg-white rounded-2xl shadow-md shadow-gray-500  right-2 py-2 px-4">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                         <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
                     </svg>
                     Show more photos</button>
             </div>
-            
+            <div className="my-2">
+                <h2 className="text-2xl my-2 semibold">Description</h2>
+                {place.description}
+            </div>
+            <div className="grid grid-cols-2">
+                <div>
+                    Check-in time: {place.checkIn}:00 <br />
+                    Check-out time: {place.checkOut}:00 <br />
+                    Max number of guests: {place.maxGuests}
+                </div>
+                <div>
+                    <div className="bg-white shadow p-4 rounded-2xl">
+                        <div className="text-2xl text-center">
+                            Price:₹{place.price} / per night
+                        </div>
+                        <div className="border rounded-2xl">
+                            <div className="flex">
+                                <div className="py-3 px-4">
+                                    <label>Check in:</label>
+                                    <input type="date"/>
+                                </div>
+                                <div className="py-3 px-4 border-l">
+                                    <label>Check Out:</label>
+                                    <input type="date"/>
+                                </div>
+                            </div>
+                            <div className="py-3 px-4 border-l">
+                                <label>Number of Guests:</label>
+                                <input type="number" value={1}/>
+                            </div>
+                        </div>
+                        <button className="primary mt-4">Book this place</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
