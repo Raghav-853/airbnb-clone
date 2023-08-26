3f4ebcff4cc1b59f0cdf8a1641f81c2ef2eaa2e5
Raghav Jethaliya <rjjethaliya853@gmail.com>
2023-08-26 17:01:34 +0530

    UserBookingPage#1

--- client/src/pages/BookingsPage.jsx
+++ client/src/pages/BookingsPage.jsx
@@ -1,7 +1,27 @@
-export default function BookingsPage(){
+import { useEffect, useState } from "react";
+import AccountNav from "../AccountNav";
+import axios from "axios";
+
+export default function BookingPage() {
+    const [bookings, setBookings] = useState([]);
+    useEffect(() => {
+        axios.get('/bookings').then(response => {
+            setBookings(response.data);
+        });
+    }, []);
     return (
         <div>
-            my bookings here
+            <AccountNav/>
+            <div>
+                {bookings?.length > 0 && bookings.map(booking => (
+                    <div>
+                        <div>
+                            
+                        </div>
+                        {booking.checkIn} to {booking.checkOut}
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
